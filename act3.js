292fd0ae473f4a71e4c321dd65da8992568f957b
1321124069 <89996865+1321124069@users.noreply.github.com>
2021-09-21 08:56:43 -0500

    Add files via upload

---
+++ act3.js
@@ -0,0 +1,2 @@
+for (o = 5; o <= 20; o=o+3)
+console.log(o)
---
+++ act5.js
@@ -0,0 +1,14 @@
+var a, b, suma, c;
+a= prompt("ingresa un numero");
+b= a;
+
+while(b = a){
+    c = prompt("ingresa otro numero");
+    a-1;
+    suma = sume + c;
+    if(b = 0){
+        console.log(suma);
+        break
+    }
+
+}
